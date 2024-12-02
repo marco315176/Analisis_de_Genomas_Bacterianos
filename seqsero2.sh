a5fc25f2b2976ad97c980f8b962333a039c9afd8
Marcopterix <91996285+marco315176@users.noreply.github.com>
2024-12-02 14:00:48 -0600

    Add files via upload

--- seqsero2.sh
+++ seqsero2.sh
@@ -8,52 +8,75 @@ echo -e                                   ===== Inicio: $(date) ===== "\n"
 
 echo -e "###################################################################################################" "\n"
 
-cd /home/admcenasa/Analisis_corridas/Resultados_all_bacteria/Ensambles/Salmonella_enterica
+cd /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_bacterial
 
+for file in /home/secuenciacion_cenasa/Analisis_corridas/kmerfinder/bacteria/*.spa; do
+    gene=$(cat ${file} | sed -n '2p' | cut -d ' ' -f '2' | tr ' ' '_')
+    organism=$(cat ${file} | sed -n '2p' | cut -d ' ' -f '2,3' | tr ' ' '_')
+    ID_org=$(basename ${file} | cut -d '_' -f '1')
 
 for assembly in *.fa; do
     ID=$(basename ${assembly} | cut -d '-' -f '1')
 
+# ----------------------------------------------------------------------------------------------------
+# Control para identificar las muestras que corresponden a Salmonella con los resultados de KmerFinder
+# ----------------------------------------------------------------------------------------------------
+
+if [[ ${ID} == ${ID_org} ]]; then
+        echo -e "If control: ${ID} ${ID_org}"
+if [[ ${gene} != "Salmonella" ]]; then
+        echo -e "${ID} encontrado como ${organism}, no encontrado como Salmonella"
+continue
+	else
+echo -e "${ID} encontrado como ${gene}" "\n"
+echo -e "###################################"
+echo -e "Corriendo SeqSero2 sobre: ${ID}"
+echo -e "###################################" "\n"
+
 # ----------------------------------------------------------------------
 # Correr SeqSero2 sobre los ensambles de Salmonella obtenidos con SPAdes
 # ----------------------------------------------------------------------
+dir="/home/secuenciacion_cenasa/Analisis_corridas/seqsero2"
 
 SeqSero2_package.py -t 4 \
                     -i ${assembly} \
                     -m k \
-                    -d /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out \
+                    -d ${dir}/${ID}_tmp_SeqSero2out \
                     -p 15
 
 # -------------------------------------------------------
 # Crear la carpeta SeqSero2_log y mover los archivos .log 
 # -------------------------------------------------------
 
-mkdir -p /home/admcenasa/Analisis_corridas/seqsero2/SeqSero2_log
+mkdir -p ${dir}/SeqSero2_log
 
-mv /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out/SeqSero_log.txt /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out/${ID}_SeqSero_log.txt
-mv /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out/${ID}_SeqSero_log.txt /home/admcenasa/Analisis_corridas/seqsero2/SeqSero2_log/.
+mv ${dir}/${ID}_tmp_SeqSero2out/SeqSero_log.txt ${dir}/${ID}_tmp_SeqSero2out/${ID}_SeqSero_log.txt
+mv ${dir}/${ID}_tmp_SeqSero2out/${ID}_SeqSero_log.txt ${dir}/SeqSero2_log/.
 
 # ------------------------------------------
 # Renombrar y mover los archivos *result.tsv
 # ------------------------------------------
 
-mv /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out/SeqSero_result.tsv /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out/${ID}_tmp_result.tsv
-mv /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_SeqSero2out/${ID}_tmp_result.tsv /home/admcenasa/Analisis_corridas/seqsero2/.
-cat /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_result.tsv | sed -e "1d" | tr ' ' '_' > /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_filt.tsv
+mv ${dir}/${ID}_tmp_SeqSero2out/SeqSero_result.tsv ${dir}/${ID}_tmp_SeqSero2out/${ID}_tmp_result.tsv
+mv ${dir}/${ID}_tmp_SeqSero2out/${ID}_tmp_result.tsv ${dir}/.
+cat ${dir}/${ID}_tmp_result.tsv | sed -e "1d" | tr ' ' '_' > ${dir}/${ID}_tmp_filt.tsv
 
 # -------------------------------------
 # Modificar los archivos .tsv de salida
 # -------------------------------------
 
-cat /home/admcenasa/Analisis_corridas/seqsero2/${ID}_tmp_filt.tsv >> /home/admcenasa/Analisis_corridas/seqsero2/SeqSero2_tmp_filt.tsv | uniq
+cat ${dir}/${ID}_tmp_filt.tsv >> ${dir}/SeqSero2_tmp_filt.tsv | uniq
 
+           fi
+	fi
+    done
 done
 
 # ------------------------------------------------------------------
 # Añadir títulos de columna e imprimir solo las columnas importantes
 # ------------------------------------------------------------------
 
-cd /home/admcenasa/Analisis_corridas/seqsero2
+cd ${dir}
 
 sed -i '1i Sample_name\tOutput_directory\tInput_files\tO_antigen_prediction\tH1_antigen_prediction(fliC)\tH2_antigen_prediction(fljB)\tPredicted_identification\tPredicted_antigenic_profile\tPredicted_serotype\tNote' ./SeqSero2_tmp_filt.tsv
 awk '{print $1"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10}' ./SeqSero2_tmp_filt.tsv > ./SeqSero2_result_filt.tsv
--- serotypefinder.sh
+++ serotypefinder.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-echo -e "#######################################################################################################" "\n"
+echo -e "#####################################################################################################" "\n"
 
-echo -e =====  Ejecución de SerotypeFinder sobre ensambles para la ideantificación de serotipos de E. coli ===== "\n"
+echo -e =====  Ejecutando SerotypeFinder sobre ensambles para la ideantificación de serotipos de E. coli ===== "\n"
 
 echo -e                                      ===== Inicio: $(date) ===== "\n"
 
-echo -e "#######################################################################################################" "\n"
+echo -e "#####################################################################################################" "\n"
 
-cd /home/admcenasa/Analisis_corridas/Resultados_all_bacteria/Ensambles/Escherichia_coli
+cd /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_bacterial
+
+for file in /home/secuenciacion_cenasa/Analisis_corridas/kmerfinder/bacteria/*.spa; do
+    gene=$(cat ${file} | sed -n '2p' | cut -d ' ' -f '2' | tr ' ' '_')
+    organism=$(cat ${file} | sed -n '2p' | cut -d ' ' -f '2,3' | tr ' ' '_')
+    ID_org=$(basename ${file} | cut -d '_' -f '1')
 
 for assembly in *.fa; do
     ID=$(basename ${assembly} | cut -d '-' -f '1')
+
+# ------------------------------------------------------------------------------------------------
+# Control para identificar las muestras que corresponden a E.coli con los resultados de KmerFinder
+# ------------------------------------------------------------------------------------------------
+
+if [[ ${ID} == ${ID_org} ]]; then
+        echo -e "If control: ${ID} ${ID_org}"
+if [[ ${organism} != "Escherichia_coli" ]]; then
+        echo -e "${ID} encontrado como ${organism}, no encontrado como Escherichia_coli"
+continue
+        else
+echo -e "${ID} encontrado como ${organism}" "\n"
+echo -e "########################################"
+echo -e "Corriendo SerotypeFinder sobre: ${ID}"
+echo -e "########################################" "\n"
 
 # -------------------------------------------------------------------------
 # Correr SerotypeFinder sobre los ensambles de E. coli obtenidos con SPAdes
 # -------------------------------------------------------------------------
 
-dir="/home/admcenasa/Analisis_corridas/serotypefinder"
+dir="/home/secuenciacion_cenasa/Analisis_corridas/serotypefinder"
 
 mkdir -p ${dir}/${ID}_tmp_SFout
 
 serotypefinder.py -i ${assembly} \
-                  -o /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_tmp_SFout \
+                  -o ${dir}/${ID}_tmp_SFout \
                   -mp blastn \
                   -p $SF_DB_PATH \
                   -x
@@ -31,26 +51,29 @@ serotypefinder.py -i ${assembly} \
 # Renombrar el archivo results_tab.tsv
 # ------------------------------------
 
-mv /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_tmp_SFout/results_tab.tsv /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_tmp_SFout/${ID}_results_tmp_SF.tsv 
-mv /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_tmp_SFout/${ID}_results_tmp_SF.tsv  /home/admcenasa/Analisis_corridas/serotypefinder/.
+mv ${dir}/${ID}_tmp_SFout/results_tab.tsv ${dir}/${ID}_tmp_SFout/${ID}_results_tmp_SF.tsv 
+mv ${dir}/${ID}_tmp_SFout/${ID}_results_tmp_SF.tsv ${dir}/.
 
 # -------------------------------------------------------------------------------------------------------
 # Imprimir solo las columnas 1,2,3 y 4 de ${ID}_results_tmp_SF.tsv y quitar el encabezado de las columnas
 # -------------------------------------------------------------------------------------------------------
 
-cat /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_results_tmp_SF.tsv | awk '{print $1"\t"$2"\t"$3"\t"$4}' | sed -e "1d" > /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_results_tmp.tsv
+cat ${dir}/${ID}_results_tmp_SF.tsv | awk '{print $1"\t"$2"\t"$3"\t"$4}' | sed -e "1d" > ${dir}/${ID}_results_tmp.tsv
 
 # ---------------------------------------------------
 # Concatenar todos los archivos de salida en uno solo
 # ---------------------------------------------------
 
-sed -i '1i Database\tGen\tAntigen_prediction\tIdentity' /home/admcenasa/Analisis_corridas/serotypefinder/${ID}_results_tmp.tsv
+sed -i '1i Database\tGen\tAntigen_prediction\tIdentity' ${dir}/${ID}_results_tmp.tsv
 
-rm -R /home/admcenasa/Analisis_corridas/serotypefinder/*tmp_SFout* /home/admcenasa/Analisis_corridas/serotypefinder/*results_tmp_SF*
+rm -R ${dir}/*tmp_SFout* ${dir}/*results_tmp_SF*
 
+	  fi
+	fi
+    done
 done
 
-cd /home/admcenasa/Analisis_corridas/serotypefinder
+cd ${dir}
 
 for file in *results_tmp*; do
     ID=$(basename ${file} | cut -d '_' -f '1')
@@ -60,7 +83,7 @@ echo -e "\n ########## \t${ID}\t ########## \n$(cat ${file})"
 done >> ./SF_results_all.tsv 
 
 
-rm /home/admcenasa/Analisis_corridas/serotypefinder/*results_tmp*
+rm ${dir}/*results_tmp*
 
 echo -e "############################################################"
 echo -e                     ===== Fin: $(date) =====
